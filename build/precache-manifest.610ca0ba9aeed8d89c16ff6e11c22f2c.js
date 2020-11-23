f66e38e9742abf634eb168191442f4223ab83aef
Fraporitmos <fran_espino189@outlook.com>
2020-11-22 19:51:03 -0600

    Updates

---
+++ build/precache-manifest.610ca0ba9aeed8d89c16ff6e11c22f2c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc7cc36672724a2037ebab77b0e2697",
+    "url": "/sgnk/index.html"
+  },
+  {
+    "revision": "dbccbb8357cc1456c366",
+    "url": "/sgnk/static/css/main.33be319f.chunk.css"
+  },
+  {
+    "revision": "25e4c5bb7ce07bebb696",
+    "url": "/sgnk/static/js/2.dcf1b49f.chunk.js"
+  },
+  {
+    "revision": "0d10b9de47c8023f7fd9d830b32d5242",
+    "url": "/sgnk/static/js/2.dcf1b49f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbccbb8357cc1456c366",
+    "url": "/sgnk/static/js/main.7e27071e.chunk.js"
+  },
+  {
+    "revision": "2f6d22a549d71ca36d3b",
+    "url": "/sgnk/static/js/runtime-main.3c89da3b.js"
+  },
+  {
+    "revision": "d22ee3727a7216019c3848df6eafa024",
+    "url": "/sgnk/static/media/github.d22ee372.png"
+  },
+  {
+    "revision": "5c570427ee23f69853d28aec805eee79",
+    "url": "/sgnk/static/media/instagram.5c570427.png"
+  },
+  {
+    "revision": "1e52e227bf5587c0f5a7980b6aac4f7d",
+    "url": "/sgnk/static/media/youtube.1e52e227.png"
+  }
+]);
